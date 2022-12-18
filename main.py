ee71a8f172b1164b422632e008097ff1ed4c40b2
Kseniia Baryshnikova <ksunny2000@yandex.ru>
2022-12-18 10:37:19 +0800

    Remove file and add working version without classes

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+# character_creation_module/main.py
+
 from random import randint
+
+# Новый импорт.
+# Из модуля start_game_banner, который расположен в папке graphic_arts,
+# импортируем функцию run_screensaver().
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Определяет и возвращает величину нанесенного противнику урона."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику'
                 f' равный {5 + randint(3, 5)}')
@@ -15,6 +23,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Определяет и возвращает величину блокированного урона."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +34,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Определяет и возвращает результат применения специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 f' «Выносливость {80 + 25}»')
@@ -36,6 +46,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Спрашивает у персонажа согласие на тренировку и проводит её."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -60,6 +71,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Позволяет выбрать и изменить класс персонажа."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -81,7 +93,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
